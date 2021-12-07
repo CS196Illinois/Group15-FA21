6052d98740202630b86079330aec80212d00f6d0
anashen <anaghas2@illinois.edu>
2021-12-06 22:52:34 -0600

    Update scripts

---
+++ Project/firebase/config.py
@@ -0,0 +1,6 @@
+config = {
+  "apiKey": "AIzaSyDAyR_1YrL771dDPIi9HERo6qVkv9VXiqM", # private (add later to local script)
+  "authDomain": "cs196-15-climate-vis.firebaseapp.com",
+  "databaseURL": "https://cs196-15-climate-vis-default-rtdb.firebaseio.com",
+  "storageBucket": "cs196-15-climate-vis.appspot.com"
+}
