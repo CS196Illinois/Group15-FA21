27d8857fa94a8dd32febf491d88d0c63b154e5c8
Anagha S <90294056+anashen@users.noreply.github.com>
2021-12-06 22:56:09 -0600

    Delete config.py

--- Project/firebase/config.py
+++
@@ -1,6 +0,0 @@
-config = {
-  "apiKey": "AIzaSyDAyR_1YrL771dDPIi9HERo6qVkv9VXiqM", # private (add later to local script)
-  "authDomain": "cs196-15-climate-vis.firebaseapp.com",
-  "databaseURL": "https://cs196-15-climate-vis-default-rtdb.firebaseio.com",
-  "storageBucket": "cs196-15-climate-vis.appspot.com"
-}
